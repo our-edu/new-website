3edcc9156456a3e23b1eb79bd52ca13ac5e50f17
korashy <ahmed.algmal@our-edu.net>
2021-09-16 16:04:04 +0200

    COM-6 added the initial bootstrapping of complains module Parent Actor

--- core/app/CommunicationApp/Complains/Models/Complain.php
+++ core/app/CommunicationApp/Complains/Models/Complain.php
@@ -5,10 +5,35 @@ declare(strict_types = 1);
 namespace App\CommunicationApp\Complains\Models;
 
 use App\BaseApp\BaseModel;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Complain extends BaseModel
 {
-    protected $fillable = [
+    use SoftDeletes;
 
+    /**
+     * @var string
+     */
+    protected $table = 'complains';
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'parent_uuid',
+        'student_uuid',
+        'body',
+        'status'
     ];
+
+    /**
+     * @return HasMany
+     */
+    public function statuses(): HasMany
+    {
+        return $this->hasMany(ComplainStatus::class,'complain_uuid','uuid');
+    }
+
+
+
 }
---
+++ core/app/CommunicationApp/Complains/Models/ComplainStatus.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\CommunicationApp\Complains\Models;
+
+use App\BaseApp\BaseModel;
+use App\BaseApp\Models\User;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ComplainStatus extends BaseModel
+{
+    use SoftDeletes;
+
+    /**
+     * @var string
+     */
+    protected $table = 'complain_statuses';
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'complain_uuid',
+        'user_uuid',
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function complain(): BelongsTo
+    {
+        return $this->belongsTo(Complain::class,'complain_uuid');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class,'user_uuid');
+    }
+
+
+}
