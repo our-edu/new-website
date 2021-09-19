a303d034df8143fe095b2eb06613831e26ccfb3e
Omar Kelany <omar.amer@our-edu.net>
2021-09-19 17:59:46 +0200

    filter announcements

--- core/app/CommunicationApp/Announcements/Repository/AnnouncementRepository.php
+++ core/app/CommunicationApp/Announcements/Repository/AnnouncementRepository.php
@@ -7,6 +7,7 @@ namespace App\CommunicationApp\Announcements\Repository;
 use App\BaseApp\Repository\Repository as RepositoryAlias;
 use App\CommunicationApp\Announcements\Models\Announcement;
 use App\CommunicationApp\Questions\Models\Question;
+use Illuminate\Database\Eloquent\Builder;
 
 class AnnouncementRepository extends RepositoryAlias implements AnnouncementRepositoryInterface
 {
@@ -18,5 +19,18 @@ class AnnouncementRepository extends RepositoryAlias implements AnnouncementRepo
     public function find($id, $columns = ['*']): Announcement
     {
         return parent::find($id, $columns);
+    }
+    public function filterData()
+    {
+        $query = $this;
+        if (request()->has('branch') && !empty(request()->get('branch'))) {
+            $query = $query->whereHas('branches', function ($q) {
+                $q->where('uuid', request()->get('branch'));
+            });
+        }
+        if (request()->has('publisher') && !empty(request()->get('publisher'))) {
+            $query = $query->where('publisher_uuid', request()->get('publisher'));
+        }
+        return $query;
     }
 }
