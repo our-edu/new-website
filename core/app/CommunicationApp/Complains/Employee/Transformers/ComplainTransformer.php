1f8c8c7ba702b3580dc38c934cfca51cda69793e
korashy <ahmed.algmal@our-edu.net>
2021-09-20 13:04:35 +0200

    COM-7 added filter for  resolve action + updated the parent user

--- core/app/CommunicationApp/Complains/Employee/Transformers/ComplainTransformer.php
+++ core/app/CommunicationApp/Complains/Employee/Transformers/ComplainTransformer.php
@@ -7,6 +7,7 @@ namespace App\CommunicationApp\Complains\Employee\Transformers;
 use App\BaseApp\Api\Enums\APIActionsEnums;
 use App\BaseApp\Enums\ResourceTypesEnums;
 use App\BaseApp\Api\Transformers\ActionTransformer;
+use App\CommunicationApp\Complains\Enums\ComplainStatusesEnum;
 use App\CommunicationApp\Complains\Models\Complain;
 use League\Fractal\TransformerAbstract;
 
@@ -42,15 +43,18 @@ class ComplainTransformer extends TransformerAbstract
 
     public function includeActions(Complain $complain)
     {
-        $actions[] = [
-            'endpoint_url' => buildScopeRoute('api.employee.complains.resolve', [
-                'complain' => $complain->uuid,
-            ]),
-            'label' => trans('questions.' . APIActionsEnums::RESOLVE_COMPLAIN),
-            'method' => 'PUT',
-            'key' => APIActionsEnums::RESOLVE_COMPLAIN
-        ];
+        $actions = [];
+        if($complain->status != ComplainStatusesEnum::RESOLVED_EN){
 
+            $actions[] = [
+                'endpoint_url' => buildScopeRoute('api.employee.complains.resolve', [
+                    'complain' => $complain->uuid,
+                ]),
+                'label' => trans('questions.' . APIActionsEnums::RESOLVE_COMPLAIN),
+                'method' => 'PUT',
+                'key' => APIActionsEnums::RESOLVE_COMPLAIN
+            ];
+        }
 
         if (count($actions)) {
             return $this->collection($actions, new ActionTransformer(), ResourceTypesEnums::ACTION);
