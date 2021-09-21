a184b228c6db60540679799d45046177a8b7fc35
korashy <ahmed.algmal@our-edu.net>
2021-09-21 13:02:27 +0200

    COM-16  Requests

---
+++ core/app/CommunicationApp/CommunicationsLogs/Visits/Employee/Requests/VisitRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\CommunicationApp\CommunicationsLogs\Visits\Employee\Requests;
+
+use App\BaseApp\Api\Requests\BaseApiRequest;
+
+class VisitRequest extends BaseApiRequest
+{
+    /**
+     * @return string[]
+     */
+    public function rules(): array
+    {
+        return [
+            'parent_uuid'  => 'required|exists:parent_users,uuid',
+            'reason'       => 'required|string',
+            'date'         => 'required|date',
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function attributes(): array
+    {
+        return [
+            'reason' => trans('visits.fields.reason'),
+            'parent_uuid' => trans('visits.fields.parent_uuid'),
+            'date' => trans('visits.fields.date')
+        ];
+    }
+}
