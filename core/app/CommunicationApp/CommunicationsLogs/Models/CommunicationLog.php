e980caa53850d72b09838baa6800d568b71183bd
korashy <ahmed.algmal@our-edu.net>
2021-09-21 12:42:50 +0200

    COM-16  create models & repositories

---
+++ core/app/CommunicationApp/CommunicationsLogs/Models/CommunicationLog.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\CommunicationApp\CommunicationsLogs\Models;
+
+use App\BaseApp\Models\Branch;
+use App\BaseApp\Models\ParentUser;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CommunicationLog extends \App\BaseApp\BaseModel
+{
+
+    use SoftDeletes;
+    protected $table = 'communications_log';
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'type',
+        'reason',
+        'parent_uuid',
+        'date',
+        'procedure',
+        'branch_uuid'
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(ParentUser::class , 'parent_uuid');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function branch(): BelongsTo
+    {
+        return $this->belongsTo(Branch::class,'branch_uuid');
+    }
+
+}
--- core/app/CommunicationApp/Complains/Repository/ComplainRepository.php
+++ core/app/CommunicationApp/Complains/Repository/ComplainRepository.php
@@ -6,6 +6,7 @@ namespace App\CommunicationApp\Complains\Repository;
 
 use App\BaseApp\Repository\Repository as RepositoryAlias;
 use App\CommunicationApp\Complains\Models\Complain;
+use Prettus\Validator\Exceptions\ValidatorException;
 
 class ComplainRepository extends RepositoryAlias implements ComplainRepositoryInterface
 {
@@ -14,14 +15,32 @@ class ComplainRepository extends RepositoryAlias implements ComplainRepositoryIn
         return Complain::class;
     }
 
+    /**
+     * @param $id
+     * @param array|string[] $columns
+     * @return Complain
+     */
     public function find($id, $columns = ['*']): Complain
     {
         return parent::find($id, $columns);
     }
+
+    /**
+     * @param array $attributes
+     * @return Complain
+     * @throws ValidatorException
+     */
     public function create(array $attributes): Complain
     {
         return parent::create($attributes);
     }
+
+    /**
+     * @param array $attributes
+     * @param $id
+     * @return Complain
+     * @throws ValidatorException
+     */
     public function update(array $attributes, $id): Complain
     {
         return  parent::update($attributes, $id);
