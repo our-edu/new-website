804b620b3fbc19bdc2796c5e232b6c54a870ff53
korashy <ahmed.algmal@our-edu.net>
2021-09-22 10:04:33 +0200

    fix the visit request

--- core/app/CommunicationApp/CommunicationsLogs/Visits/Employee/Requests/VisitRequest.php
+++ core/app/CommunicationApp/CommunicationsLogs/Visits/Employee/Requests/VisitRequest.php
@@ -14,7 +14,7 @@ class VisitRequest extends BaseApiRequest
     public function rules(): array
     {
         return [
-            'git '  => 'required|exists:parent_users,uuid',
+            'parent_uuid'  => 'required|exists:parent_users,uuid',
             'reason'       => 'required|string',
             'date'         => 'required|date',
         ];
