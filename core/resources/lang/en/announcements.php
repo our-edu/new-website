d278431572425a9625b00d738b59a62cf4a008d6
korashy <ahmed.algmal@our-edu.net>
2021-10-04 16:10:48 +0200

    COMِ-ATEF update filter label translations

---
+++ core/resources/lang/en/announcements.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    "filters"=>
+            [
+                'label' =>[
+                    'branches' =>'Select Branch',
+                    'publisher' => 'Select Publisher',
+
+                ]
+            ]
+];
