87aa6c5fa57a1120be6ba5ac004f7e9e6ff07131
korashy <ahmed.algmal@our-edu.net>
2021-10-06 12:27:08 +0200

    COMِ-66 events now is filtered by period

--- core/app/CommunicationApp/Events/Repository/EventRepository.php
+++ core/app/CommunicationApp/Events/Repository/EventRepository.php
@@ -22,20 +22,9 @@ class EventRepository extends RepositoryAlias implements EventRepositoryInterfac
         return parent::find($id, $columns);
     }
 
-    public function filterData()
+    public function filterData($start,$end)
     {
-        $query = $this;
-        switch (request()->get('period')) {
-            case 'week':
-                $query = $query->whereBetween('start', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()]);
-                break;
-            case 'day':
-                $query = $query->whereDay('start', Carbon::now()->day);
-                break;
-            default:
-                $query = $query->whereMonth('start', Carbon::now()->month);
-                break;
-        }
+        $query = $this->where('start',">=",$start)->where('end',"<=",$end);
         return $query;
     }
 
