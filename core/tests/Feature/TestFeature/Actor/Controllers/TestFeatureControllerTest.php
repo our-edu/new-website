88068c03b89517f01206ed931e891298ecc9fda5
korashy <ahmed.algmal@our-edu.net>
2021-11-08 15:06:12 +0200

    add example Test

---
+++ core/tests/Feature/TestFeature/Actor/Controllers/TestFeatureControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature\TestFeature\Actor\Controllers;
+
+
+use Tests\TestCase;
+
+class  TestFeatureControllerTest extends TestCase
+{
+    /**
+     * @test
+     */
+
+    public function list_resource_name()
+    {
+        $this->assertTrue(true);
+
+    }
+
+
+
+
+}
+
