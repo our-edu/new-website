cdd7f09dcd0ff74d3a532884bbdbd4435a7b94d8
korashy <ahmed.algmal@our-edu.net>
2021-11-10 12:59:35 +0200

    BAC-192 sniffer fixes

--- core/routes/web.php
+++ core/routes/web.php
@@ -16,9 +16,8 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 Route::group([
         'prefix' => LaravelLocalization::setLocale(),
         'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
-    ], function(){
-    Route::get('/', function () {
-        return view('welcome');
+    ], function () {
+        Route::get('/', function () {
+            return view('welcome');
+        });
     });
-
-});
