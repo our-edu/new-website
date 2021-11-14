b21323f144397ec8a3b008540833a7c67c9e0fff
korashy <ahmed.algmal@our-edu.net>
2021-11-14 11:24:01 +0200

    added the payment status enums

---
+++ core/app/BaseApp/Enums/PayFortStatusEnum.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\PaymentApp\Payments\Enums;
+
+abstract class PayFortStatusEnum
+{
+    const TOKENIZATION_SUCCESS = '02';
+    const INVALID_REQUEST = '00';
+    const ON_HOLD = '20';
+    const PURCHASE_SUCCESS = '14';
+    const SUCCESS = '02';
+    const CAPTURE_SUCCESS = '04';
+}
