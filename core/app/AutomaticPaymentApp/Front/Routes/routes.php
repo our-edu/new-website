865aa0d3afdd1a58e1e1f678204c02547e3268d7
korashy <ahmed.algmal@our-edu.net>
2021-11-14 11:54:47 +0200

    added routes for getting the form and parsing it a head

--- core/app/AutomaticPaymentApp/Front/Routes/routes.php
+++ core/app/AutomaticPaymentApp/Front/Routes/routes.php
@@ -5,4 +5,6 @@ Route::group(['namespace'=>'\App\AutomaticPaymentApp\Front\Controllers'],functio
         return view('welcome')->with(['loginEnabled'=>false,'registerEnabled'=>false]);
     });
     Route::post('/','FrontController@searchNationalId')->name('searchNationalId');
+    Route::get('/due_payment/{national_id}','FrontController@getPaymentView')->name('getPaymentView');
+    Route::post('/due_payment_form/','FrontController@getPaymentForm')->name('getPaymentForm');
 });
