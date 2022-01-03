0465f7b8687de42bef5692eccedd8d8440fb0558
tawakl <tawakl@ouredu.com>
2022-01-03 14:41:08 +0200

    validation

--- core/app/NewWebsiteApp/Admin/Events/Requests/UpdateEventRequest.php
+++ core/app/NewWebsiteApp/Admin/Events/Requests/UpdateEventRequest.php
@@ -6,26 +6,26 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateEventRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
-//            'title' => 'required|max:250',
-//            'description' => 'required',
+            'title' => 'required|max:250',
+            'description' => 'required',
+            'event_date' => 'required',
+            'start_time' => 'required ',
+            'end_time' => 'required |after:start_time',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.required' => 'الاسم مطلوب',
+            'description.required' => 'الوصف مطلوب',
+            'event_date.required' => 'هذا الحقل مطلوب',
+            'start_time.required' => 'هذا الحقل مطلوب',
+            'end_time.required' => 'هذا الحقل مطلوب',
+            'end_time.after' => 'هذا الحقل يجب ان يكون لاحقا لوقت البدء',
         ];
     }
 }
--- core/app/NewWebsiteApp/Admin/Videos/Requests/UpdateVideosRequest.php
+++ core/app/NewWebsiteApp/Admin/Videos/Requests/UpdateVideosRequest.php
@@ -6,26 +6,21 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateVideosRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
-//            'title' => 'required|max:250',
-//            'description' => 'required',
+            'title' => 'required|max:250',
+            'description' => 'required',
+            'video_url' => 'required|url',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.required' => 'العنوان مطلوب',
+            'description.required' => 'الوصف مطلوب',
+            'video_url.required' => 'هذا الحقل مطلوب',
         ];
     }
 }
