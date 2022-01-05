fb19de87f6f13b22a36dfeb3d9d4a2ea3b8a2819
yasmin <yasminmostafasmsm2010@gmail.com>
2022-01-05 11:38:18 +0200

    fix admin style

--- core/app/NewWebsiteApp/Front/Routes/routes.php
+++ core/app/NewWebsiteApp/Front/Routes/routes.php
@@ -6,11 +6,4 @@ Route::group(['namespace'=>'\App\AutomaticPaymentApp\Front\Controllers'], functi
     Route::get('/', function () {
         return view('Front.index');
     })->name('home');
-    Route::post('/', 'FrontController@searchNationalId')->name('searchNationalId');
-    Route::group(['prefix'=>'payment' ,'as'=>'payments'], function () {
-        Route::get('/due_payment/{national_id}', 'FrontController@getPaymentView')->name('.getPaymentView');
-        Route::post('/due_payment/', 'FrontController@getPaymentForm')->name('.getPaymentForm');
-        Route::post('/process-return', 'FrontController@processReturn')->name('.processReturn');
-        Route::get('/processpdf', 'FrontController@processReturnPdf')->name('.processReturnPdf');
-    });
 });
