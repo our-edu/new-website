5786d57ad07ef9299eacbbad71a2b7c4a80e1d3f
yasmin <yasminmostafasmsm2010@gmail.com>
2022-01-26 10:05:37 +0200

    upload front

--- core/resources/views/Front/partials/navigation.blade.php
+++ core/resources/views/Front/partials/navigation.blade.php
@@ -40,7 +40,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="{{route('articles')}}">
+            <a class="nav-link" href="{{route('researches')}}">
               درسات و بحوث
             </a>
           </li>
