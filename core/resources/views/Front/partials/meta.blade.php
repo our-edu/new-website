4591ed02c839453717e6952dc857fb3092811f13
yasmin <yasminmostafasmsm2010@gmail.com>
2022-01-30 11:02:45 +0200

    fix

--- core/resources/views/Front/partials/meta.blade.php
+++ core/resources/views/Front/partials/meta.blade.php
@@ -1,6 +1,5 @@
 <meta name="keywords" content="{{$row->meta()->count() > 0 ? $row->meta->meta_keys : null}}">
 <meta name="description" content="{{$row->meta()->count() > 0 ? $row->meta->meta_description : null}}">
-//og data
 <meta property=”og:title” content=”{{$row->title}}” />
 <meta property=”og:url” content=”{{$route}}” />
 <meta property=”og:type” content=”website” />
