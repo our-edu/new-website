1fad60f0bba38ab007322a7bfa384c77e898c6b0
yasmin <yasminmostafasmsm2010@gmail.com>
2022-01-30 15:15:15 +0200

    fix

--- core/resources/views/Front/pages/contact_us.blade.php
+++ core/resources/views/Front/pages/contact_us.blade.php
@@ -60,10 +60,7 @@
       <section class="embed-responsive embed-responsive-21by9 contact_map">
         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3625.029983476847!2d46.596669!3d24.691496!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe6d023cc9113cee5!2sIbn%20Khaldoun%20School%20for%20Boys!5e0!3m2!1sen!2ssa!4v1643548401506!5m2!1sen!2ssa"
                 width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26432.42324808999!2d-118.34398767954286!3d34.09378509738966!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2bf07045279bf%3A0xf67a9a6797bdfae4!2sHollywood%2C%20Los%20Angeles%2C%20CA%2C%20USA!5e0!3m2!1sen!2sbd!4v1576846473265!5m2!1sen!2sbd"
-          width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen=""
-          class="embed-responsive-item"></iframe>
+
         <div id="over_map">
           <div class="item">
             <span>الرياض :  ص.ب :230310</span>
